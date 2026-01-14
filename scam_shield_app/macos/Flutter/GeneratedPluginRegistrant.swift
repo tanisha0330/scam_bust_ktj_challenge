c196b655186f190022a915d56232f93ff1cef63f
tanisha0330 <tanishajaiswal20042004@gmail.com>
2026-01-15 02:32:56 +0530

    2nd feature implemented

--- scam_shield_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ scam_shield_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
